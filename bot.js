ed9f873ac35a07fb9da05111a7893b876b10134c
yousefsa <45287171+yousefsa@users.noreply.github.com>
2019-02-08 13:36:28 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 var prefix = "M";
 var adminprefix = 'M'
 
-const developers = ["537783570941476864"]
+const developers = ["412547940654252053"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
